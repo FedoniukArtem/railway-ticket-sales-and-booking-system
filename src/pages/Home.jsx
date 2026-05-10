1dd020706762cec4ff7ff610b777e592bad69f8d
Artem Fedoniuk web2025-1 <iiilssofff@gmail.com>
2026-05-11 01:31:46 +0300

    додав файл Home.jsx виведення всього на головну сторінку

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,13 @@
-﻿
+﻿import React from 'react';
+import TrainList from '../components/TrainList';
+
+const Home = () => {
+    return (
+        <main style={{ padding: '20px' }}>
+            <h1>Розклад потягів (Укрзалізниця)</h1>
+            <TrainList />
+        </main>
+    );
+};
+
+export default Home;
