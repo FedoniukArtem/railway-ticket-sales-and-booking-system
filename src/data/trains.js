3b8754fb89b7fd77d29c30120cde89a70862d76a
Artem Fedoniuk web2025-1 <iiilssofff@gmail.com>
2026-05-11 01:24:16 +0300

    Додав початкові дані поїзда

--- src/data/trains.js
+++ src/data/trains.js
@@ -1,1 +1,23 @@
-// JavaScript source code
+export const trains = [
+    {
+        id: 1,
+        number: "001К",
+        route: { from: "Київ", to: "Львів" },
+        departure: "2026-05-20T22:30:00",
+        duration: "7г 15хв"
+    },
+    {
+        id: 2,
+        number: "705К",
+        route: { from: "Київ", to: "Пшемисль" },
+        departure: "2026-05-21T06:00:00",
+        duration: "9г 00хв"
+    },
+    {
+        id: 3,
+        number: "063Х",
+        route: { from: "Харків", to: "Київ" },
+        departure: "2026-05-20T21:15:00",
+        duration: "8г 30хв"
+    }
+];
